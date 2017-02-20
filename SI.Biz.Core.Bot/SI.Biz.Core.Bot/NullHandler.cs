37062884410b545d2dade73ac399c421dfc8160b
ap\ramadhar <harsha.ramachandrarao@tieto.com>
2017-02-19 22:20:43 -0800

    Re-added missed classes from project

---
+++ SI.Biz.Core.Bot/SI.Biz.Core.Bot/NullHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SI.Biz.Core.Bot
+{
+    public static class Empty<T> where T : new()
+    {
+        public static List<T> DefaultList()
+        {
+            return new List<T>();
+        }
+
+        public static T Default()
+        {
+            return new T();
+        }
+    }
+}
