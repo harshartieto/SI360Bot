55624471de315ae9ba3d96220514dbb06f1e145a
ap\ramadhar <harsha.ramachandrarao@tieto.com>
2017-02-19 22:40:43 -0800

    BL case unit test

---
+++ SI.Biz.Core.Bot/SI.Biz.Core.Bot.UnitTest/Case/CaseManagerTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SI.Biz.Core.Bot.InvokeWrapper.Case;
+using SI.Biz.Core.Bot.InvokeWrapper.FakeImplementation.Case;
+
+namespace SI.Biz.Core.Bot.UnitTest.Case
+{
+    [TestClass]
+    public class CaseManagerTests
+    {
+
+        private CaseManagerInvoker _caseManagerInvoker;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _caseManagerInvoker = new CaseManagerInvoker(TestConstants.EndPointUri, TestConstants.User);
+        }
+
+        [TestMethod]
+        public void FindCaseMyNumber_Finds360CaseByItsNumber_ReturnsBotCaseObject()
+        {
+            var result = _caseManagerInvoker.FindCaseByNumber("12");
+            Assert.IsNotNull(result);
+            //Assert.IsTrue(!string.IsNullOrEmpty(caseDetail));
+        }
+    }
+}
