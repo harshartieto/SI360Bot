fc83592e8fc2656fa5e376deab2af259c6c119e9
harshartieto <harsha.ramachandrarao@tieto.com>
2017-02-19 23:15:23 -0800

    Add files via upload

---
+++ SI.Biz.Core.Bot/SI.Biz.Core.Bot/_Compatibility/Case/BotCaseManager.cs
@@ -0,0 +1,25 @@
+﻿
+using SI.Biz.Core.Compatibility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SI.Biz.Core.Bot.Compatibility
+{
+    public class BotCaseManager : LegacyManager
+    {
+        public string FindCaseByNumber(string id, string caseNumber)
+        {
+            var botCase = Invoke(id, () => SI.Biz.Core.Bot.BotCaseManager.FindCaseByNumber(caseNumber));
+            return string.Empty;
+        }
+
+        public string GetMyOpenCases(string id, string userId)
+        {
+            var botCase = Invoke(id, () =>  SI.Biz.Core.Bot.BotCaseManager.GetMyOpenCases(userId));
+            return string.Empty;
+        }
+    }
+}
