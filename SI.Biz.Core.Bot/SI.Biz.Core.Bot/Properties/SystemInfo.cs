f23bbe1fdc2df45c203fdf53153abb291fd7a53f
ap\ramadhar <harsha.ramachandrarao@tieto.com>
2017-02-21 22:19:34 -0800

    System Info added

---
+++ SI.Biz.Core.Bot/SI.Biz.Core.Bot/Properties/SystemInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System;
+using System.Resources;
+
+//
+// Common assembly info 
+//
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Software Innovation")]
+[assembly: AssemblyProduct("SI360")]
+[assembly: AssemblyCopyright("Software Innovation AS")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+//
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+
+#if !ASSEMBLY_VERSION_OVERRIDE
+
+[assembly: AssemblyVersion("4.0.0.0")]
+#endif
+
+[assembly: AssemblyFileVersion("4.1.9020.0331")]
+
+[assembly: AssemblyDelaySign(false)]
+
+[assembly: AssemblyInformationalVersion("4.1 SP9 FP02")]
